bb68fe570da23023e4d26909078ca0a5588ff762
cris <criseustache44@gmail.com>
2020-02-26 12:14:33 -0600

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :comments
+  devise_for :users
+  resources :recipes
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'recipes#index'
+
+  get 'pages/about'
+end
