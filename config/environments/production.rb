94563c327c78acb7bbb1784860bce178a77c7386
cris <criseustache44@gmail.com>
2020-03-03 13:57:55 -0600

    reverted production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,8 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  APPLICATIONNAME::Application.configure do
-    config.assets.compile = true
-  end
-  
+
 end
